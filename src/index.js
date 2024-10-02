003c5970076d0801732d3e414c32ef36f5c42ef1
Lily <mrs.esmirn@gmail.com>
2024-10-02 10:23:32 +0400

    add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+  let str = n.toString();
+  let arr = [];
+
+  for (i = 0; i < str.length; i++) {
+        arr.push(str[i]);
+  }
+
+  let reversed = arr.reverse();
+
+  return +reversed.join('');
+
 }
